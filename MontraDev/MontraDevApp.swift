33679e15991fa0622d62c4afc448b03063829a46
Maya <Mayastridevana0205@gmail.com>
2024-04-18 11:34:08 +0700

    Initial Commit

---
+++ MontraDev/MontraDevApp.swift
@@ -0,0 +1,17 @@
+//
+//  MontraDevApp.swift
+//  MontraDev
+//
+//  Created by User on 18/04/24.
+//
+
+import SwiftUI
+
+@main
+struct MontraDevApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
